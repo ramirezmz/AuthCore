a4a5c9e65950f753b8545dac59fdee0c58fe4071
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2025-08-21 02:31:28 -0300

    style(linter): :lipstick: run linter

--- adapters/auth/jwt_auth_service.py
+++ adapters/auth/jwt_auth_service.py
@@ -4,7 +4,9 @@ from config.settings import Settings
 
 
 def generate_token(user):
-    expiration_time = datetime.utcnow() + timedelta(hours=Settings.JWT_EXP_HOURS)
+    expiration_time = datetime.utcnow() + timedelta(
+        hours=Settings.JWT_EXP_HOURS
+    )
     payload = {
         "sub": user.id,
         "email": user.email,
@@ -13,7 +15,8 @@ def generate_token(user):
         "iat": datetime.utcnow(),
     }
 
-    token = jwt.encode(payload, Settings.JWT_SECRET, algorithm=Settings.JWT_ALGORITHM)
+    token = jwt.encode(payload, Settings.JWT_SECRET,
+                       algorithm=Settings.JWT_ALGORITHM)
     return token
 
 
